bf81aa48ff2ec94ededab7f65ced4d4336ae16f1
Lane Lu <161492667+llucn@users.noreply.github.com>
2025-03-11 15:18:05 +0800

    chore: initial commit

---
+++ ios/PhotoEdit.swift
@@ -0,0 +1,8 @@
+@objc(PhotoEdit)
+class PhotoEdit: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
